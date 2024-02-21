fd5b861ceffe568467c57e648f1af61641419afa
natesanow <152323081+natesanow@users.noreply.github.com>
2024-02-21 14:13:07 -0800

    changed file destination for generated readme

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((answers) => {
         console.log(answers);
-        writeToFile("README.md", answers);
+        writeToFile("./utils/README.md", answers);
     });
 }
 
