3f33fe29082fffefe6e1e7c189a908c576fd9597
natesanow <152323081+natesanow@users.noreply.github.com>
2024-02-21 13:59:45 -0800

    added to render license section function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,7 +6,7 @@ function renderLicenseBadge(license) {
     if (license) {
         return ``;
     } else {
-        return `[![${license} license](https://img.shields.io/badge/license-${license}-blue.svg)](${renderLincenseLink(license)})`
+        return `[![${license} license](https://img.shields.io/badge/license-${license}-blue.svg)](${renderLicenseLink(license)})`
     }
 }
 
@@ -38,7 +38,13 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license) {
+        return ``;
+    } else {
+        return `This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -49,6 +55,7 @@ ${data.description}
 ## Table of Contents
 * [Installation](#installation)
 * [Usage](#usage)
+* [License] (#license)
 * [Contributions](#contributions)
 * [Tests](#tests)
 * [Questions](#questions)
@@ -56,6 +63,8 @@ ${data.description}
 ${data.installation}
 ## Usage
 ${data.usage}
+## License
+${renderLicenseSection(data.license)}
 ## Contributions
 ${data.contributions}
 ## Tests
